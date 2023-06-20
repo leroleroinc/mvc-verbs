fcd6987a814e1ecc254d9343771552bfac98bee4
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2023-06-20 18:29:07 -0300

    add Eureka Discovery Client

--- src/main/java/com/lerolero/verbs/VerbsApplication.java
+++ src/main/java/com/lerolero/verbs/VerbsApplication.java
@@ -2,8 +2,10 @@ package com.lerolero.verbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VerbsApplication {
 
 	public static void main(String[] args) {
